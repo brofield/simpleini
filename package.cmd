bd09bea9248d51c86e87dbd3a5a97de0ce77f2d5
Brodie Thiesfield <brofield@gmail.com>
2008-04-24 02:47:30 +0000

    package latest changes into a release

---
+++ package.cmd
@@ -0,0 +1,23 @@
+set VERSION=4.6
+
+set SEVENZIP="C:\Program Files\7-Zip\7z.exe"
+
+FOR /F "tokens=*" %%G IN ('DIR /AD /B /S Debug*') DO (
+    DEL /S /Q "%%G"
+    RD "%%G"
+)
+FOR /F "tokens=*" %%G IN ('DIR /AD /B /S Release*') DO (
+    DEL /S /Q "%%G"
+    RD "%%G"
+)
+DEL /Q "SimpleIni.ncb"
+ATTRIB -H "SimpleIni.suo"
+DEL /Q "SimpleIni.suo"
+DEL /Q "SimpleIni.opt"
+START "Generate documentation" /WAIT SimpleIni.doxy
+cd ..
+del simpleini-%VERSION%.zip
+%SEVENZIP% a -tzip -r- -x!simpleini\.svn simpleini-%VERSION%.zip simpleini\*
+del simpleini-doc.zip
+%SEVENZIP% a -tzip -r simpleini-doc.zip simpleini-doc\*
+cd simpleini
