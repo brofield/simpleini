aaeb2c8e558fa3167237983a2e66d6c8d3563d54
brofield <brofield@users.noreply.github.com>
2026-06-15 13:52:29 +1000

    auto formatting of the code

--- tests/ts-roundtrip.cpp
+++ tests/ts-roundtrip.cpp
@@ -1,246 +1,226 @@
-﻿#include <algorithm>
-#include "../SimpleIni.h"
+﻿#include "../SimpleIni.h"
 #include "gtest/gtest.h"
+#include <algorithm>
 
 class TestRoundTrip : public ::testing::Test {
 protected:
-	void SetUp() override;
-	void TestMulti();
-	void TestBOM(bool useBOM);
+  void SetUp() override;
+  void TestMulti();
+  void TestBOM(bool useBOM);
 
 protected:
-	CSimpleIniA ini;
-	std::string input;
-	std::string output;
+  CSimpleIniA ini;
+  std::string input;
+  std::string output;
 };
 
-void TestRoundTrip::SetUp() {
-	ini.SetUnicode();
-}
+void TestRoundTrip::SetUp() { ini.SetUnicode(); }
 
 TEST_F(TestRoundTrip, TestStandard) {
-	input =
-		"; File comment\n"
-		"\n"
-		"\n"
-		"; Section 1 comment\n"
-		"[section1]\n"
-		"\n"
-		"\n"
-		"; Section 2 comment\n"
-		"[section2]\n"
-		"\n"
-		"; key1 comment\n"
-		"key1 = string\n"
-		"\n"
-		"; key 2 comment\n"
-		"key2 = true\n"
-		"key3 = 3.1415\n"
-		;
+  input = "; File comment\n"
+          "\n"
+          "\n"
+          "; Section 1 comment\n"
+          "[section1]\n"
+          "\n"
+          "\n"
+          "; Section 2 comment\n"
+          "[section2]\n"
+          "\n"
+          "; key1 comment\n"
+          "key1 = string\n"
+          "\n"
+          "; key 2 comment\n"
+          "key2 = true\n"
+          "key3 = 3.1415\n";
 
-	SI_Error rc = ini.LoadData(input);
-	ASSERT_EQ(rc, SI_OK);
+  SI_Error rc = ini.LoadData(input);
+  ASSERT_EQ(rc, SI_OK);
 
-	const char* result = ini.GetValue("section2", "key1");
-	ASSERT_STREQ(result, "string");
+  const char *result = ini.GetValue("section2", "key1");
+  ASSERT_STREQ(result, "string");
 
-	rc = ini.Save(output);
-	ASSERT_EQ(rc, SI_OK);
+  rc = ini.Save(output);
+  ASSERT_EQ(rc, SI_OK);
 
-	output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
-	ASSERT_STREQ(input.c_str(), output.c_str());
+  output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
+  ASSERT_STREQ(input.c_str(), output.c_str());
 }
 
 void TestRoundTrip::TestMulti() {
-	input =
-		"[section]\n"
-		"key = string1\n"
-		"key = string2\n"
-		;
+  input = "[section]\n"
+          "key = string1\n"
+          "key = string2\n";
 
-	SI_Error rc = ini.LoadData(input);
-	ASSERT_EQ(rc, SI_OK);
+  SI_Error rc = ini.LoadData(input);
+  ASSERT_EQ(rc, SI_OK);
 
-	rc = ini.Save(output);
-	ASSERT_EQ(rc, SI_OK);
+  rc = ini.Save(output);
+  ASSERT_EQ(rc, SI_OK);
 
-	output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
+  output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
 }
 
 TEST_F(TestRoundTrip, TestMultiGood) {
-	ini.SetMultiKey(true);
-	TestMulti();
-	ASSERT_STREQ(input.c_str(), output.c_str());
+  ini.SetMultiKey(true);
+  TestMulti();
+  ASSERT_STREQ(input.c_str(), output.c_str());
 }
 
 TEST_F(TestRoundTrip, TestMultiBad) {
-	std::string expected =
-		"[section]\n"
-		"key = string2\n";
+  std::string expected = "[section]\n"
+                         "key = string2\n";
 
-	ini.SetMultiKey(false);
-	TestMulti();
-	ASSERT_STRNE(input.c_str(), output.c_str());
-	ASSERT_STREQ(expected.c_str(), output.c_str());
+  ini.SetMultiKey(false);
+  TestMulti();
+  ASSERT_STRNE(input.c_str(), output.c_str());
+  ASSERT_STREQ(expected.c_str(), output.c_str());
 }
 
 TEST_F(TestRoundTrip, TestSpacesTrue) {
-	input =
-		"[section]\n"
-		"key = string1\n";
+  input = "[section]\n"
+          "key = string1\n";
 
-	SI_Error rc = ini.LoadData(input);
-	ASSERT_EQ(rc, SI_OK);
+  SI_Error rc = ini.LoadData(input);
+  ASSERT_EQ(rc, SI_OK);
 
-	ini.SetSpaces(true);
-	rc = ini.Save(output);
-	ASSERT_EQ(rc, SI_OK);
+  ini.SetSpaces(true);
+  rc = ini.Save(output);
+  ASSERT_EQ(rc, SI_OK);
 
-	output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
-	
-	ASSERT_STREQ(input.c_str(), output.c_str());
+  output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
+
+  ASSERT_STREQ(input.c_str(), output.c_str());
 }
 
 TEST_F(TestRoundTrip, TestSpacesFalse) {
-	input =
-		"[section]\n"
-		"key = string1\n";
+  input = "[section]\n"
+          "key = string1\n";
 
-	SI_Error rc = ini.LoadData(input);
-	ASSERT_EQ(rc, SI_OK);
+  SI_Error rc = ini.LoadData(input);
+  ASSERT_EQ(rc, SI_OK);
 
-	ini.SetSpaces(false);
-	rc = ini.Save(output);
-	ASSERT_EQ(rc, SI_OK);
+  ini.SetSpaces(false);
+  rc = ini.Save(output);
+  ASSERT_EQ(rc, SI_OK);
 
-	output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
+  output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
 
-	ASSERT_STRNE(input.c_str(), output.c_str());
+  ASSERT_STRNE(input.c_str(), output.c_str());
 
-	std::string expected =
-		"[section]\n"
-		"key=string1\n";
+  std::string expected = "[section]\n"
+                         "key=string1\n";
 
-	ASSERT_STREQ(expected.c_str(), output.c_str());
+  ASSERT_STREQ(expected.c_str(), output.c_str());
 }
 
 void TestRoundTrip::TestBOM(bool useBOM) {
-	const char bom[] = "\xEF\xBB\xBF";
-	const char input8[] =
-		u8"[テスト1]\n"
-		u8"テスト2 = テスト3\n";
+  const char bom[] = "\xEF\xBB\xBF";
+  const char input8[] = u8"[テスト1]\n"
+                        u8"テスト2 = テスト3\n";
 
-	input = bom;
-	input += input8;
+  input = bom;
+  input += input8;
 
-	ini.Reset();
-	ini.SetUnicode(false);
-	SI_Error rc = ini.LoadData(input);
-	ASSERT_EQ(rc, SI_OK);
+  ini.Reset();
+  ini.SetUnicode(false);
+  SI_Error rc = ini.LoadData(input);
+  ASSERT_EQ(rc, SI_OK);
 
-	const char tesuto1[] = u8"テスト1";
-	const char tesuto2[] = u8"テスト2";
-	const char tesuto3[] = u8"テスト3";
+  const char tesuto1[] = u8"テスト1";
+  const char tesuto2[] = u8"テスト2";
+  const char tesuto3[] = u8"テスト3";
 
-	const char* result = ini.GetValue(tesuto1, tesuto2);
-	ASSERT_STREQ(result, tesuto3);
+  const char *result = ini.GetValue(tesuto1, tesuto2);
+  ASSERT_STREQ(result, tesuto3);
 
-	rc = ini.Save(output, useBOM);
-	ASSERT_EQ(rc, SI_OK);
+  rc = ini.Save(output, useBOM);
+  ASSERT_EQ(rc, SI_OK);
 
-	output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
+  output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
 }
 
 TEST_F(TestRoundTrip, TestWithBOM) {
-	TestBOM(true);
+  TestBOM(true);
 
-	ASSERT_STREQ(input.c_str(), output.c_str());
+  ASSERT_STREQ(input.c_str(), output.c_str());
 }
 
 TEST_F(TestRoundTrip, TestWithoutBOM) {
-	TestBOM(false);
+  TestBOM(false);
 
-	ASSERT_STRNE(input.c_str(), output.c_str());
+  ASSERT_STRNE(input.c_str(), output.c_str());
 
-	std::string expected(input, 3);
-	ASSERT_STREQ(expected.c_str(), output.c_str());
+  std::string expected(input, 3);
+  ASSERT_STREQ(expected.c_str(), output.c_str());
 }
 
 TEST_F(TestRoundTrip, TestAllowKeyOnly1) {
-	ini.SetAllowKeyOnly(false);
+  ini.SetAllowKeyOnly(false);
 
-	input =
-		"[section1]\n"
-		"key1 = string\n"
-		"key2 = \n"
-		"key3= \n"
-		"key4=\n"
-		"key5\n"
-		"\n"
-		"Never going to give you up\n"
-		"Never going to let you down\n"
-		;
+  input = "[section1]\n"
+          "key1 = string\n"
+          "key2 = \n"
+          "key3= \n"
+          "key4=\n"
+          "key5\n"
+          "\n"
+          "Never going to give you up\n"
+          "Never going to let you down\n";
 
-	std::string expect =
-		"[section1]\n"
-		"key1 = string\n"
-		"key2 = \n"
-		"key3 = \n"
-		"key4 = \n"
-		;
+  std::string expect = "[section1]\n"
+                       "key1 = string\n"
+                       "key2 = \n"
+                       "key3 = \n"
+                       "key4 = \n";
 
-	SI_Error rc = ini.LoadData(input);
-	ASSERT_EQ(rc, SI_OK);
+  SI_Error rc = ini.LoadData(input);
+  ASSERT_EQ(rc, SI_OK);
 
-	rc = ini.Save(output);
-	ASSERT_EQ(rc, SI_OK);
+  rc = ini.Save(output);
+  ASSERT_EQ(rc, SI_OK);
 
-	output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
-	ASSERT_STREQ(expect.c_str(), output.c_str());
+  output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
+  ASSERT_STREQ(expect.c_str(), output.c_str());
 }
 
 TEST_F(TestRoundTrip, TestAllowKeyOnly2) {
-	ini.SetAllowKeyOnly(true);
+  ini.SetAllowKeyOnly(true);
 
-	input =
-		"[section1]\n"
-		"key1\n"
-		"key2\n"
-		"[section2]\n"
-		"key1 = string\n"
-		"key2 = \n"
-		"key3= \n"
-		"key4=\n"
-		"\n"
-		"key5\n"
-		"\n"
-		"Never going to give you up\n"
-		"\n"
-		"Never going to let you down\n"
-		;
+  input = "[section1]\n"
+          "key1\n"
+          "key2\n"
+          "[section2]\n"
+          "key1 = string\n"
+          "key2 = \n"
+          "key3= \n"
+          "key4=\n"
+          "\n"
+          "key5\n"
+          "\n"
+          "Never going to give you up\n"
+          "\n"
+          "Never going to let you down\n";
 
-	std::string expect =
-		"[section1]\n"
-		"key1\n"
-		"key2\n"
-		"\n\n"
-		"[section2]\n"
-		"key1 = string\n"
-		"key2\n"
-		"key3\n"
-		"key4\n"
-		"key5\n"
-		"Never going to give you up\n"
-		"Never going to let you down\n"
-		;
+  std::string expect = "[section1]\n"
+                       "key1\n"
+                       "key2\n"
+                       "\n\n"
+                       "[section2]\n"
+                       "key1 = string\n"
+                       "key2\n"
+                       "key3\n"
+                       "key4\n"
+                       "key5\n"
+                       "Never going to give you up\n"
+                       "Never going to let you down\n";
 
-	SI_Error rc = ini.LoadData(input);
-	ASSERT_EQ(rc, SI_OK);
+  SI_Error rc = ini.LoadData(input);
+  ASSERT_EQ(rc, SI_OK);
 
-	rc = ini.Save(output);
-	ASSERT_EQ(rc, SI_OK);
+  rc = ini.Save(output);
+  ASSERT_EQ(rc, SI_OK);
 
-	output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
-	ASSERT_STREQ(expect.c_str(), output.c_str());
+  output.erase(std::remove(output.begin(), output.end(), '\r'), output.end());
+  ASSERT_STREQ(expect.c_str(), output.c_str());
 }
-
--- tests/ts-snippets.cpp
+++ tests/ts-snippets.cpp
@@ -1,303 +1,291 @@
 #include "../SimpleIni.h"
 #include "gtest/gtest.h"
 
-
 // ### SIMPLE USAGE
 
 TEST(TestSnippets, TestSimple) {
-	// simple demonstration
+  // simple demonstration
 
-	CSimpleIniA ini;
-	ini.SetUnicode();
+  CSimpleIniA ini;
+  ini.SetUnicode();
 
-	SI_Error rc = ini.LoadFile("example.ini");
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_OK);
+  SI_Error rc = ini.LoadFile("example.ini");
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_OK);
 
-	const char* pv;
-	pv = ini.GetValue("section", "key", "default");
-	ASSERT_STREQ(pv, "value");
+  const char *pv;
+  pv = ini.GetValue("section", "key", "default");
+  ASSERT_STREQ(pv, "value");
 
-	ini.SetValue("section", "key", "newvalue");
+  ini.SetValue("section", "key", "newvalue");
 
-	pv = ini.GetValue("section", "key", "default");
-	ASSERT_STREQ(pv, "newvalue");
+  pv = ini.GetValue("section", "key", "default");
+  ASSERT_STREQ(pv, "newvalue");
 }
-
 
 // ### LOADING DATA
 
 TEST(TestSnippets, TestLoadFile) {
-	// load from a data file
-	CSimpleIniA ini;
-	SI_Error rc = ini.LoadFile("example.ini");
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_OK);
+  // load from a data file
+  CSimpleIniA ini;
+  SI_Error rc = ini.LoadFile("example.ini");
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_OK);
 }
 
 TEST(TestSnippets, TestLoadString) {
-	// load from a string
-	const std::string example = "[section]\nkey = value\n";
-	CSimpleIniA ini;
-	SI_Error rc = ini.LoadData(example);
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_OK);
+  // load from a string
+  const std::string example = "[section]\nkey = value\n";
+  CSimpleIniA ini;
+  SI_Error rc = ini.LoadData(example);
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_OK);
 }
-
 
 // ### GETTING SECTIONS AND KEYS
 
 TEST(TestSnippets, TestSectionsAndKeys) {
-	const std::string example =
-		"[section1]\n"
-		"key1 = value1\n"
-		"key2 = value2\n"
-		"\n"
-		"[section2]\n"
-		"[section3]\n";
+  const std::string example = "[section1]\n"
+                              "key1 = value1\n"
+                              "key2 = value2\n"
+                              "\n"
+                              "[section2]\n"
+                              "[section3]\n";
 
-	CSimpleIniA ini;
-	SI_Error rc = ini.LoadData(example);
-	ASSERT_EQ(rc, SI_OK);
-	
+  CSimpleIniA ini;
+  SI_Error rc = ini.LoadData(example);
+  ASSERT_EQ(rc, SI_OK);
 
+  // get all sections
+  CSimpleIniA::TNamesDepend sections;
+  ini.GetAllSections(sections);
 
-	// get all sections
-	CSimpleIniA::TNamesDepend sections;
-	ini.GetAllSections(sections);
+  // get all keys in a section
+  CSimpleIniA::TNamesDepend keys;
+  ini.GetAllKeys("section1", keys);
 
-	// get all keys in a section
-	CSimpleIniA::TNamesDepend keys;
-	ini.GetAllKeys("section1", keys);
+  const char *expectedSections[] = {"section1", "section2", "section3",
+                                    nullptr};
+  const char *expectedKeys[] = {"key1", "key2", nullptr};
 
+  CSimpleIniA::TNamesDepend::const_iterator it;
+  int i;
 
+  for (i = 0, it = sections.begin(); it != sections.end(); ++i, ++it) {
+    ASSERT_NE(expectedSections[i], nullptr);
+    ASSERT_STREQ(expectedSections[i], it->pItem);
+  }
+  ASSERT_EQ(expectedSections[i], nullptr);
 
-	const char* expectedSections[] = { "section1", "section2", "section3", nullptr };
-	const char* expectedKeys[] = { "key1", "key2", nullptr };
-
-	CSimpleIniA::TNamesDepend::const_iterator it;
-	int i;
-
-	for (i = 0, it = sections.begin(); it != sections.end(); ++i, ++it) {
-		ASSERT_NE(expectedSections[i], nullptr);
-		ASSERT_STREQ(expectedSections[i], it->pItem);
-	}
-	ASSERT_EQ(expectedSections[i], nullptr);
-
-	for (i = 0, it = keys.begin(); it != keys.end(); ++i, ++it) {
-		ASSERT_NE(expectedKeys[i], nullptr);
-		ASSERT_STREQ(expectedKeys[i], it->pItem);
-	}
-	ASSERT_EQ(expectedKeys[i], nullptr);
+  for (i = 0, it = keys.begin(); it != keys.end(); ++i, ++it) {
+    ASSERT_NE(expectedKeys[i], nullptr);
+    ASSERT_STREQ(expectedKeys[i], it->pItem);
+  }
+  ASSERT_EQ(expectedKeys[i], nullptr);
 }
-
 
 // ### GETTING VALUES
 
 TEST(TestSnippets, TestGettingValues) {
-	const std::string example =
-		"[section1]\n"
-		"key1 = value1\n"
-		"key2 = value2.1\n"
-		"key2 = value2.2\n"
-		"\n"
-		"[section2]\n"
-		"[section3]\n";
+  const std::string example = "[section1]\n"
+                              "key1 = value1\n"
+                              "key2 = value2.1\n"
+                              "key2 = value2.2\n"
+                              "\n"
+                              "[section2]\n"
+                              "[section3]\n";
 
-	bool utf8 = true;
-	bool multiKey = true;
-	CSimpleIniA ini(utf8, multiKey);
-	SI_Error rc = ini.LoadData(example);
-	ASSERT_EQ(rc, SI_OK);
+  bool utf8 = true;
+  bool multiKey = true;
+  CSimpleIniA ini(utf8, multiKey);
+  SI_Error rc = ini.LoadData(example);
+  ASSERT_EQ(rc, SI_OK);
 
+  // get the value of a key that doesn't exist
+  const char *pv;
+  pv = ini.GetValue("section1", "key99");
+  ASSERT_EQ(pv, nullptr);
 
-	// get the value of a key that doesn't exist
-	const char* pv;
-	pv = ini.GetValue("section1", "key99");
-	ASSERT_EQ(pv, nullptr);
+  // get the value of a key that does exist
+  pv = ini.GetValue("section1", "key1");
+  ASSERT_STREQ(pv, "value1");
 
-	// get the value of a key that does exist
-	pv = ini.GetValue("section1", "key1");
-	ASSERT_STREQ(pv, "value1");
+  // get the value of a key which may have multiple
+  // values. If hasMultiple is true, then there are
+  // multiple values and just one value has been returned
+  bool hasMulti;
+  pv = ini.GetValue("section1", "key1", nullptr, &hasMulti);
+  ASSERT_STREQ(pv, "value1");
+  ASSERT_EQ(hasMulti, false);
 
-	// get the value of a key which may have multiple 
-	// values. If hasMultiple is true, then there are
-	// multiple values and just one value has been returned
-	bool hasMulti;
-	pv = ini.GetValue("section1", "key1", nullptr, &hasMulti);
-	ASSERT_STREQ(pv, "value1");
-	ASSERT_EQ(hasMulti, false);
+  pv = ini.GetValue("section1", "key2", nullptr, &hasMulti);
+  ASSERT_STREQ(pv, "value2.1");
+  ASSERT_EQ(hasMulti, true);
 
-	pv = ini.GetValue("section1", "key2", nullptr, &hasMulti);
-	ASSERT_STREQ(pv, "value2.1");
-	ASSERT_EQ(hasMulti, true);
+  // get all values of a key with multiple values
+  CSimpleIniA::TNamesDepend values;
+  ini.GetAllValues("section1", "key2", values);
 
-	// get all values of a key with multiple values
-	CSimpleIniA::TNamesDepend values;
-	ini.GetAllValues("section1", "key2", values);
+  // sort the values into a known order, in this case we want
+  // the original load order
+  values.sort(CSimpleIniA::Entry::LoadOrder());
 
-	// sort the values into a known order, in this case we want
-	// the original load order
-	values.sort(CSimpleIniA::Entry::LoadOrder());
+  // output all of the items
+  CSimpleIniA::TNamesDepend::const_iterator it;
+  for (it = values.begin(); it != values.end(); ++it) {
+    //printf("value = '%s'\n", it->pItem);
+  }
 
-	// output all of the items
-	CSimpleIniA::TNamesDepend::const_iterator it;
-	for (it = values.begin(); it != values.end(); ++it) {
-		//printf("value = '%s'\n", it->pItem);
-	}
-
-
-	int i;
-	const char* expectedValues[] = { "value2.1", "value2.2", nullptr };
-	for (i = 0, it = values.begin(); it != values.end(); ++it, ++i) {
-		ASSERT_NE(expectedValues[i], nullptr);
-		ASSERT_STREQ(expectedValues[i], it->pItem);
-	}
-	ASSERT_EQ(expectedValues[i], nullptr);
+  int i;
+  const char *expectedValues[] = {"value2.1", "value2.2", nullptr};
+  for (i = 0, it = values.begin(); it != values.end(); ++it, ++i) {
+    ASSERT_NE(expectedValues[i], nullptr);
+    ASSERT_STREQ(expectedValues[i], it->pItem);
+  }
+  ASSERT_EQ(expectedValues[i], nullptr);
 }
 
 // ### VALUE EXISTS
 
-TEST(TestSnippets, TestExists)
-{
-	const std::string example =
-		"[section1]\n"
-		"key1 = value1\n"
-		"key2 = value2.1\n"
-		"key2 = value2.2\n"
-		"\n"
-		"[section2]\n"
-		"key1\n"
-		"key2\n"
-		"[section3]\n";
+TEST(TestSnippets, TestExists) {
+  const std::string example = "[section1]\n"
+                              "key1 = value1\n"
+                              "key2 = value2.1\n"
+                              "key2 = value2.2\n"
+                              "\n"
+                              "[section2]\n"
+                              "key1\n"
+                              "key2\n"
+                              "[section3]\n";
 
-	CSimpleIniA ini;
-	ini.SetUnicode();
-	ini.SetMultiKey();
-	ini.SetAllowKeyOnly();
+  CSimpleIniA ini;
+  ini.SetUnicode();
+  ini.SetMultiKey();
+  ini.SetAllowKeyOnly();
 
-	SI_Error rc = ini.LoadData(example);
-	ASSERT_EQ(rc, SI_OK);
+  SI_Error rc = ini.LoadData(example);
+  ASSERT_EQ(rc, SI_OK);
 
+  // check for section doesn't exist
+  EXPECT_FALSE(ini.SectionExists(""));
+  EXPECT_FALSE(ini.SectionExists("section4"));
 
-	// check for section doesn't exist
-	EXPECT_FALSE(ini.SectionExists(""));
-	EXPECT_FALSE(ini.SectionExists("section4"));
+  // check for section does exist
+  EXPECT_TRUE(ini.SectionExists("section1"));
+  EXPECT_TRUE(ini.SectionExists("section2"));
+  EXPECT_TRUE(ini.SectionExists("section3"));
 
-	// check for section does exist
-	EXPECT_TRUE(ini.SectionExists("section1"));
-	EXPECT_TRUE(ini.SectionExists("section2"));
-	EXPECT_TRUE(ini.SectionExists("section3"));
+  // check for key doesn't exist
+  EXPECT_FALSE(ini.KeyExists("", "key"));
+  EXPECT_FALSE(ini.KeyExists("section1", "key"));
+  EXPECT_FALSE(ini.KeyExists("section2", "key"));
 
-	// check for key doesn't exist
-	EXPECT_FALSE(ini.KeyExists("", "key"));
-	EXPECT_FALSE(ini.KeyExists("section1", "key"));
-	EXPECT_FALSE(ini.KeyExists("section2", "key"));
-
-	// check for key does exist
-	EXPECT_TRUE(ini.KeyExists("section1", "key1"));
-	EXPECT_TRUE(ini.KeyExists("section1", "key2"));
-	EXPECT_TRUE(ini.KeyExists("section2", "key1"));
-	EXPECT_TRUE(ini.KeyExists("section2", "key2"));
+  // check for key does exist
+  EXPECT_TRUE(ini.KeyExists("section1", "key1"));
+  EXPECT_TRUE(ini.KeyExists("section1", "key2"));
+  EXPECT_TRUE(ini.KeyExists("section2", "key1"));
+  EXPECT_TRUE(ini.KeyExists("section2", "key2"));
 }
 
 // ### MODIFYING DATA
 
 TEST(TestSnippets, TestModifyingData) {
-	bool utf8 = true;
-	bool multiKey = false;
-	CSimpleIniA ini(utf8, multiKey);
-	SI_Error rc;
+  bool utf8 = true;
+  bool multiKey = false;
+  CSimpleIniA ini(utf8, multiKey);
+  SI_Error rc;
 
+  // add a new section
+  rc = ini.SetValue("section1", nullptr, nullptr);
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_INSERTED);
 
-	// add a new section 
-	rc = ini.SetValue("section1", nullptr, nullptr);
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_INSERTED); 
+  // not an error to add one that already exists
+  rc = ini.SetValue("section1", nullptr, nullptr);
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_UPDATED);
 
-	// not an error to add one that already exists
-	rc = ini.SetValue("section1", nullptr, nullptr);
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_UPDATED);
+  // get the value of a key that doesn't exist
+  const char *pv;
+  pv = ini.GetValue("section2", "key1", "default-value");
+  ASSERT_STREQ(pv, "default-value");
 
-	// get the value of a key that doesn't exist
-	const char* pv;
-	pv = ini.GetValue("section2", "key1", "default-value");
-	ASSERT_STREQ(pv, "default-value");
+  // adding a key (the section will be added if needed)
+  rc = ini.SetValue("section2", "key1", "value1");
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_INSERTED);
 
-	// adding a key (the section will be added if needed)
-	rc = ini.SetValue("section2", "key1", "value1");
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_INSERTED);
+  // ensure it is set to expected value
+  pv = ini.GetValue("section2", "key1", nullptr);
+  ASSERT_STREQ(pv, "value1");
 
-	// ensure it is set to expected value
-	pv = ini.GetValue("section2", "key1", nullptr);
-	ASSERT_STREQ(pv, "value1");
+  // change the value of a key
+  rc = ini.SetValue("section2", "key1", "value2");
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_UPDATED);
 
-	// change the value of a key
-	rc = ini.SetValue("section2", "key1", "value2");
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_UPDATED);
-
-	// ensure it is set to expected value
-	pv = ini.GetValue("section2", "key1", nullptr);
-	ASSERT_STREQ(pv, "value2");
+  // ensure it is set to expected value
+  pv = ini.GetValue("section2", "key1", nullptr);
+  ASSERT_STREQ(pv, "value2");
 }
-
 
 // ### DELETING DATA
 
 TEST(TestSnippets, TestDeletingData) {
-	const std::string example =
-		"[section1]\n"
-		"key1 = value1\n"
-		"key2 = value2\n"
-		"\n"
-		"[section2]\n"
-		"key1 = value1\n"
-		"key2 = value2\n"
-		"\n"
-		"[section3]\n";
+  const std::string example = "[section1]\n"
+                              "key1 = value1\n"
+                              "key2 = value2\n"
+                              "\n"
+                              "[section2]\n"
+                              "key1 = value1\n"
+                              "key2 = value2\n"
+                              "\n"
+                              "[section3]\n";
 
-	bool utf8 = true;
-	CSimpleIniA ini(utf8);
-	SI_Error rc = ini.LoadData(example);
-	ASSERT_EQ(rc, SI_OK);
+  bool utf8 = true;
+  CSimpleIniA ini(utf8);
+  SI_Error rc = ini.LoadData(example);
+  ASSERT_EQ(rc, SI_OK);
 
+  // deleting a key from a section. Optionally the entire
+  // section may be deleted if it is now empty.
+  bool done, deleteSectionIfEmpty = true;
+  done = ini.Delete("section1", "key1", deleteSectionIfEmpty);
+  ASSERT_EQ(done, true);
+  done = ini.Delete("section1", "key1");
+  ASSERT_EQ(done, false);
 
-	// deleting a key from a section. Optionally the entire
-	// section may be deleted if it is now empty.
-	bool done, deleteSectionIfEmpty = true;
-	done = ini.Delete("section1", "key1", deleteSectionIfEmpty);
-	ASSERT_EQ(done, true);
-	done = ini.Delete("section1", "key1");
-	ASSERT_EQ(done, false);
-
-	// deleting an entire section and all keys in it
-	done = ini.Delete("section2", nullptr);
-	ASSERT_EQ(done, true);
-	done = ini.Delete("section2", nullptr);
-	ASSERT_EQ(done, false);
+  // deleting an entire section and all keys in it
+  done = ini.Delete("section2", nullptr);
+  ASSERT_EQ(done, true);
+  done = ini.Delete("section2", nullptr);
+  ASSERT_EQ(done, false);
 }
-
 
 // ### SAVING DATA
 
 TEST(TestSnippets, TestSavingData) {
-	bool utf8 = true;
-	CSimpleIniA ini(utf8);
-	SI_Error rc;
+  bool utf8 = true;
+  CSimpleIniA ini(utf8);
+  SI_Error rc;
 
+  // save the data to a string
+  std::string data;
+  rc = ini.Save(data);
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_OK);
 
-	// save the data to a string
-	std::string data;
-	rc = ini.Save(data);
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_OK);
-
-	// save the data back to the file
-	rc = ini.SaveFile("example2.ini");
-	if (rc < 0) { /* handle error */ };
-	ASSERT_EQ(rc, SI_OK);
+  // save the data back to the file
+  rc = ini.SaveFile("example2.ini");
+  if (rc < 0) { /* handle error */
+  };
+  ASSERT_EQ(rc, SI_OK);
 }
-
